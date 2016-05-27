253699b1588702a9701bb5c9e7c7e18caebb6886
Nick Phillips <phillipn101@gmail.com>
2016-05-27 08:51:13 -0500

    Add validations to our models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,13 @@
 class Restaurant < ActiveRecord::Base
-	mount_uploader :image, ImageUploader
-	validates :name, length: {maximum: 30}, presence: true, uniqueness: true
-	validates :address, length: {maximum: 40}, presence: true
-	validates :phone, length: {maximum: 10}, presence: true, format: {with: /\A\d+\z/, message: "needs digits only"}
-	has_many :reviews
+  mount_uploader :image, ImageUploader
+
+  has_many :reviews
+
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ActiveRecord::Base
-	validates :rating, presence: true, format: {with: /\A[12345]\z/, message: "1-5 only"}
-	validates :comment, presence: true, length: {maximum: 140}
-	belongs_to :user
-	belongs_to :restaurant
+  belongs_to :user
+  belongs_to :restaurant
+
+  validates :rating, :comment, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    message: "can only be a whole number between 1 and 5"
+  }
 end
